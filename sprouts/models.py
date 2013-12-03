281c2836730c82186298b95475834b5c98416268
Joseph Edwards VIII <joseph8th@notroot.us>
2013-12-03 16:49:59 -0700

    update

--- sprouts/models.py
+++ sprouts/models.py
@@ -5,15 +5,33 @@ VTX_TOKEN = ','
 
 
 
+class Seed(object):
+    """
+    A class to encapsulate 'seed' vertex data.
+    """
+
+    value = None
+    edges = 0
+
+    def __init__(self, value, edges=0):
+        self.value = value
+        self.edges = edges
+
+    def __repr__(self):
+        return str(self.value)
+
+
+
 class Position(object):
     """
     A class to encapsulate gameboard position data.
     """
 
     faces = None
+    edges_dict = {}
 
-    def __init__(self, args):
-        self.faces = self._init_faces(faces)               # a 'public' attribute
+    def __init__(self, input_str):
+        self.faces = self.get_faces_list(input_str)
 
 
     def __str__(self):
@@ -24,7 +42,7 @@ class Position(object):
         return self.get_faces_string()
 
 
-    def _init_faces(self, input_str):
+    def get_faces_list(self, input_str):
         # 'Private' method to parse string argument into list of lists of lists.
         
         faces_l = []
@@ -33,10 +51,14 @@ class Position(object):
             bound_l = []
             faces_l.append(bound_l)
             for bnd in face.strip().split(BND_TOKEN):
-                vtx_l = []
-                bound_l.append(vtx_l)
-                for vtx in bnd.strip().split(VTX_TOKEN):
-                    vtx_l.append(vtx.strip())
+                seed_l = []
+                bound_l.append(seed_l)
+                vtx_l = bnd.strip().split(VTX_TOKEN)
+                for vtx_ix in range(len(vtx_l)):
+                    vtx = vtx_l[vtx_ix].strip()
+#                    seed = Seed(vtx)
+                    if vtx in [s.value for s in bound_l]:
+                    seed_l.append(seed)
 
         return faces_l
 
@@ -52,7 +74,7 @@ class Position(object):
             for bnd_ix in range(len(face)):
                 bnd = face[bnd_ix]
                 for vtx_ix in range(len(bnd)):
-                    faces_s += bnd[vtx_ix]
+                    faces_s += str(bnd[vtx_ix])
                     faces_s += VTX_TOKEN if vtx_ix < len(bnd)-1 else ''
                 faces_s += BND_TOKEN if bnd_ix < len(face)-1 else ''
             faces_s += FACE_TOKEN if face_ix < len(self.faces)-1 else ''
@@ -68,15 +90,24 @@ class GameTree(object):
 
     def __init__(self, init_position):
         self.root = init_position
-        self.tree = self.gen_gametree()
+        self.tree = self.gen_tree()
 
 
     def get_current_position(self):
-        return self.game_tree[-1:][-1:]
+        return self.tree[-1][-1]
                     
 
-    def gen_gametree(self):
-        pass
+    def gen_tree(self):
+        tree = [[self.root],]
+
+        for face in tree[-1][-1].faces:
+            for start_bnd in face:
+                for end_bnd in face:
+                    for start_vtx in start_bnd:
+                        for end_vtx in end_bnd:
+                            print "New edge: (%s,%s)" % (start_vtx, end_vtx)
+
+        return tree
 
 
 
